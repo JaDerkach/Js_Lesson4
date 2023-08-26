400627c2d6cacd72d2b1036fa1c6f5b653ee9b7d
Iaroslav Derkach <141680960+JaDerkach@users.noreply.github.com>
2023-08-26 23:31:50 +0300

    add users

--- hw4.js
+++ hw4.js
@@ -7,6 +7,25 @@
 // - створити функцію яка приймає масив та виводить кожен його елемент
 
 
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+function looper(Array) {
+    for (let users of Array) {
+        console.log(users)
+    } 
+}
+looper(users)
 // - створити функцію яка створює параграф з текстом. Текст задати через аргумент;
 function writ(history) {
     document.write(`
